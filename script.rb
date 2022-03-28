9c0ad672b49edeacd31d0445c9188f27c13e85bb
Josh McLeod <josh@mcleodfamily.org>
2022-03-28 12:54:27 -0400

    Add a binding

--- script.rb
+++ script.rb
@@ -6,7 +6,7 @@ words = CSV.open('./words.csv').read.first
 blacklist = %w[a i t]
 
 possible_words = []
-
+binding.pry
 # store only words that do not contain any letters in the blacklist array
 
 puts possible_words
