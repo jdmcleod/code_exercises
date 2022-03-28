bbe34f3c0a03235b8c0a87a8b004fc785fd4b01e
Josh McLeod <josh@mcleodfamily.org>
2022-03-28 12:39:16 -0400

    Initial commit for wordle exercise

---
+++ script.rb
@@ -0,0 +1,12 @@
+require 'csv'
+require 'pry'
+
+words = CSV.open('./words.csv').read.first
+
+blacklist = %w[a i t]
+
+possible_words = []
+
+# store only words that do not contain any letters in the blacklist array
+
+puts possible_words
