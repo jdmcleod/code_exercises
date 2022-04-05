a57c03f27ac907bb0c2a2375014d770bfd49fc26
Josh McLeod <josh@mcleodfamily.org>
2022-04-05 12:45:22 -0400

    Call worldle solve in script

--- script.rb
+++ script.rb
@@ -1,7 +1,8 @@
 require_relative './wordle_solver'
 
-blacklist = %w[g u i d e t o r y c m p]
-yellows = {  }
-greens = { s: 0, h: 1, a: 2}
+blacklist = %w[a d i o n y l n e i c t p t j]
+yellows = { u: [1], r: [3]  }
+greens = { r: 1, u: 2, s: 3}
 
 wordle_solver = WordleSolver.new(blacklist, yellows, greens)
+wordle_solver.solve
