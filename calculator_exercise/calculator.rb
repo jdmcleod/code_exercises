3b35de23f5a3e28382d79b777ce1f76dabb9e12b
Joby Martin <jobymartin@Jobys-MacBook-Pro.local>
2022-05-06 14:17:06 -0400

    added the subtract, multiply, and square_root methods to the Calculator class.

--- calculator_exercise/calculator.rb
+++ calculator_exercise/calculator.rb
@@ -2,4 +2,16 @@ class Calculator
   def add(num1, num2)
     num1 + num2
   end
+
+  def subtract(num1, num2)
+    num1 - num2
+  end
+
+  def multiply(num1, num2)
+    num1 * num2
+  end
+
+  def square_root(number)
+    Math.sqrt(number)
+  end
 end
