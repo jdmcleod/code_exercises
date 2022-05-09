6b8d5ea99204167257e764f553d2c914ba7b9a9c
JobyMartin <102612692+JobyMartin@users.noreply.github.com>
2022-05-09 12:10:33 -0400

    Joby finishing calculator exercise (#2)
    * add method
    
    * added the subtract, multiply, and square_root methods to the Calculator class.
    
    * Add test for divide and divide method
    
    Co-authored-by: Joby Martin <jobymartin@Jobys-MacBook-Pro.local>

--- calculator_exercise/calculator.rb
+++ calculator_exercise/calculator.rb
@@ -1,3 +1,21 @@
 class Calculator
-  # put code here
+  def add(num1, num2)
+    num1 + num2
+  end
+
+  def subtract(num1, num2)
+    num1 - num2
+  end
+
+  def multiply(num1, num2)
+    num1 * num2
+  end
+
+  def square_root(number)
+    Math.sqrt(number)
+  end
+
+  def divide(num1, num2)
+    num1 / num2
+  end
 end
