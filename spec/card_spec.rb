2564230d04f45222b35e743e74906140bd16ca61
Caleb McLeod <calebmcleod@Calebs-MBP-2016.lan>
2022-06-03 15:24:12 -0400

    FIx assertions in card spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -25,25 +25,25 @@ RSpec.describe 'word collector' do
     it 'return true if the value of the card is higher than another card' do
       card1 = Card.new('5', 'clubs')
       card2 = Card.new('3', 'clubs')
-      expect(card1.beats(card2)).to be_true
+      expect(card1.beats(card2)).to eq true
     end
 
     it 'return true if the value of the card is higher than another card' do
       card1 = Card.new('king', 'clubs')
       card2 = Card.new('queen', 'clubs')
-      expect(card1.beats(card2)).to be_true
+      expect(card1.beats(card2)).to eq true
     end
 
     it 'return false if the value of the card is lower than another card' do
       card1 = Card.new('ace', 'clubs')
       card2 = Card.new('jack', 'clubs')
-      expect(card1.beats(card2)).to be_false
+      expect(card1.beats(card2)).to eq false
     end
 
     it 'return false if the value of the card is lower than another card' do
       card1 = Card.new('3', 'clubs')
       card2 = Card.new('4', 'clubs')
-      expect(card1.beats(card2)).to be_false
+      expect(card1.beats(card2)).to eq false
     end
   end
 end
